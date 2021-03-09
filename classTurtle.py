5746310d4b6ba72d03e0b6bb6c4bdbaf7656728d
Leo Quezada <74736115+leezadda@users.noreply.github.com>
2021-03-08 19:52:14 -0600

    Add files via upload

---
+++ classTurtle.py
@@ -0,0 +1,28 @@
+import turtle
+
+window = turtle.Screen()
+steve = turtle.Turtle()
+
+#L
+steve.right(90)
+steve.forward(100)
+steve.left(90)
+steve.forward(50)
+
+#E
+steve.penup()
+steve.right(90)
+steve.forward(100)
+steve.pendown()
+steve.left(90)
+steve.forward(50)
+steve.left(90)
+steve.forward(50)
+steve.right(90)
+steve.forward(50)
+steve.left(90)
+steve.forward(50)
+#O
+
+
+turtle.mainloop()
